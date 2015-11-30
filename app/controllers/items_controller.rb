d634226d27c8a26d3e4a3fec37dd1bcfb9ad5c38
papapin <papapin777@gmail.com>
2015-11-30 21:53:03 +0200

    step 17

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -67,12 +67,11 @@ class ItemsController < ApplicationController
 	private
 
 		def find_item
-			@item = Item.find(params[:id])
+			@item = Item.where(id: params[:id]).first
+			render_404 unless @item
 		end
 
-		def check_if_admin
-			#render text: "Access denied", status: 403 unless params[:admin]			
-		end
+
 
 
 end
